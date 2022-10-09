600388afcc45771f01e4cbcadb67fc70ffd5dc13
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-09 14:34:36 -0300

    create a question to user
    Faz uma pergunta para o usuário

---
+++ Atividades Node.js (11-07-2022)/atividade_if_básico_2.js
@@ -0,0 +1,7 @@
+// Atividade Node.js
+// 2 - Faça um programa que receba um número do usuário e verifique se este número é par ou ímpar, escrevendo na tela "PAR" ou "IMPAR".
+
+const rs = require("readline-sync")
+
+console.log("Digite um número para saber se é par ou impar:")
+let number = rs.questionInt(" >> ")
