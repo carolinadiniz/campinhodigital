81bb2aa3b2ad7bf34fd6b792b7fd623890e7e13b
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-09 17:12:48 -0300

    Create Atividade1.js

---
+++ Atividades Condicional Avançada (18-07-2022)/Atividade1.js
@@ -0,0 +1,4 @@
+// Atividade Node.js
+// 1 - Elabore um algoritmo que receba dois números e determine qual é o maior enntre eles, se os números forem iguais, mostre uma mensagem no console "Os números são iguais"
+
+// import modules
