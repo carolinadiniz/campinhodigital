79002e3c5d8ae7d939397307a822593e1eeffe0b
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-09 14:21:44 -0300

    Importing module readline-sync

---
+++ Atividades Node.js (11-07-2022)/atividade_if_básico_1.js
@@ -0,0 +1,5 @@
+// Atividade Node.js  - IF Básico
+// 1 - Escreva um programa para ler o ano de nascimento de uma pessoa e escrever uma mensagem que 
+// diga se ela poderá ou não votar este ano (não é necessário considerar o mês em que ela nasceu).
+
+const rs = require("readline-sync")
