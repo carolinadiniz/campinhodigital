fd90f1d930784c9232eb16e50d768c7a28e4eba7
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-09 17:13:33 -0300

    import readlinesync module

--- Atividades Condicional Avançada (18-07-2022)/Atividade1.js
+++ Atividades Condicional Avançada (18-07-2022)/Atividade1.js
@@ -2,3 +2,5 @@
 // 1 - Elabore um algoritmo que receba dois números e determine qual é o maior enntre eles, se os números forem iguais, mostre uma mensagem no console "Os números são iguais"
 
 // import modules
+const rs = require("readline-sync")
+
