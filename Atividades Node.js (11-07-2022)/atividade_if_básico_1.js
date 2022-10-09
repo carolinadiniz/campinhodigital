f18b47b227a00052795a869ec1e9ed81d614bd07
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-09 14:24:16 -0300

    Get current year and user year of birth

--- Atividades Node.js (11-07-2022)/atividade_if_básico_1.js
+++ Atividades Node.js (11-07-2022)/atividade_if_básico_1.js
@@ -2,4 +2,12 @@
 // 1 - Escreva um programa para ler o ano de nascimento de uma pessoa e escrever uma mensagem que 
 // diga se ela poderá ou não votar este ano (não é necessário considerar o mês em que ela nasceu).
 
+// Importing modules
 const rs = require("readline-sync")
+
+// Get current year
+const current_year = new Date().getFullYear()
+
+// Get user year of birth
+let year_of_birth = rs.questionInt("Digite seu ano de nascimento: ")
+
