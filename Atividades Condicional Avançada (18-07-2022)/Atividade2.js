478a4d1797436e889204c385470a7aa960f3533c
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-09 17:24:53 -0300

    add a input number from user

--- Atividades Condicional Avançada (18-07-2022)/Atividade2.js
+++ Atividades Condicional Avançada (18-07-2022)/Atividade2.js
@@ -1,2 +1,11 @@
 // Atividade Node.js
 // 2 - Elabore um algoritmo que receba um número (1-7) e devolva o dia da semana correspondente.
+
+// import modules
+const rs = require('readline-sync')
+
+// Ask to user a number btw 1-7
+console.clear()
+console.log("Digite um número (1-7):")
+let num = rs.questionInt(">> ")
+
