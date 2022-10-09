04f4ed9b078f1eb1133cf526ab58b9c3eca7017c
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-09 18:07:58 -0300

    fix DT calculation

--- Atividades Condicional Avançada (18-07-2022)/Atividade3.js
+++ Atividades Condicional Avançada (18-07-2022)/Atividade3.js
@@ -13,7 +13,7 @@ console.log("Digite a velocidade média esperada para a viagem: km/h")
 let vm = rs.questionInt(" >> ")
 
 // calcule (DT = Delta T to minutes)
-DT = 119//(distance/vm)*60
+DT = (distance/vm)*60
 time = [parseInt(DT/60),(DT%60)]
 
 console.log(`Você levará aproximadamente ${time[0]}h ${time[1]}min para chegar ao destino.`)
