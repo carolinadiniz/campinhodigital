6554c20195cfb55c1fe65981a7ffd718324bef1e
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-09 17:28:33 -0300

    add a switch to chose a day of week

--- Atividades Condicional Avançada (18-07-2022)/Atividade2.js
+++ Atividades Condicional Avançada (18-07-2022)/Atividade2.js
@@ -9,3 +9,30 @@ console.clear()
 console.log("Digite um número (1-7):")
 let num = rs.questionInt(">> ")
 
+console.log("")
+switch (num) {
+    case 1:
+        console.log("Domingo")
+        break;
+    case 2:
+        console.log("Segunda-feira")
+        break;
+    case 3:
+        console.log("Terça-feira")
+        break;
+    case 4:
+        console.log("Quarta-feira")
+        break;
+    case 5:
+        console.log("Quinta-feira")
+        break;
+    case 6:
+        console.log("Sexta-feira")
+        break;
+    case 7:
+        console.log("Sábado")
+        break;
+    default:
+        console.log("Número digitado inválido!")
+        break;
+}
