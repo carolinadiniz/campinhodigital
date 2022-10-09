be4c8f52c48bf580ed78e840031fbac3c4f0da11
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-09 17:41:19 -0300

    add question of distance and vm

--- Atividades Condicional Avançada (18-07-2022)/Atividade3.js
+++ Atividades Condicional Avançada (18-07-2022)/Atividade3.js
@@ -1,3 +1,13 @@
 // Atividade Node.js
 // Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
 
+// import modules
+const rs = require("readline-sync")
+
+console.log("\nCALCULO DE TEMPO\n")
+console.log("Digite a distância a ser percorrida:")
+let distance = rs.questionInt(" >> ")
+
+console.log("Digite a velocidade média esperada para a viagem: km/h")
+let vm = rs.questionInt(" >> ")
+
