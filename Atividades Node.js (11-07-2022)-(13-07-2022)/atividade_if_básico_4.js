26a8a463746465c1aac366b5a5b9bb30fb49fab4
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-09 16:07:57 -0300

    Create atividade_if_básico_4.js

---
+++ Atividades Node.js (11-07-2022)-(13-07-2022)/atividade_if_básico_4.js
@@ -0,0 +1,3 @@
+// Atividade Node.js
+// 4 - As maçãs custam R$0,30 cada se forem compradas menos do que 12, e R$0,25 se forem compradas 12 ou mais. Escreva um programa que leia o número de maçãs compradas, calcule e escreva o valor total da compra.
+
