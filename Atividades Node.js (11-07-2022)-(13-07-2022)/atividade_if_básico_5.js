73b71ac6f18d85e356bd8c4e064a5c85be874d19
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-09 16:21:06 -0300

    import modules

--- Atividades Node.js (11-07-2022)-(13-07-2022)/atividade_if_básico_5.js
+++ Atividades Node.js (11-07-2022)-(13-07-2022)/atividade_if_básico_5.js
@@ -1,3 +1,6 @@
 // Atividades Node.js
 // 5 - Escreva um programa para ler 3 valores inteiros (considere que não serão lidos valores iguais) e escrevê-los em ordem crescente.
 
+// import modules
+const rs = require("readline-sync")
+const bar = require("./bar")
