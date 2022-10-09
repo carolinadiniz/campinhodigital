2746267e2e384cffe47d0ecc1968673b6e9a6c3b
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-09 16:17:03 -0300

    fix and clear console

--- Atividades Node.js (11-07-2022)-(13-07-2022)/atividade_if_básico_4.js
+++ Atividades Node.js (11-07-2022)-(13-07-2022)/atividade_if_básico_4.js
@@ -6,7 +6,8 @@ const rs = require("readline-sync")
 const bar = require("./bar")
 
 // ask how much apples were bought
-console.log("CALCULO DE MAÇÃS\NDIGITE O NÚMERO DE MAÇÃS COMPRADAS:")
+console.clear()
+console.log("CALCULO DE MAÇÃS\n\nDIGITE O NÚMERO DE MAÇÃS COMPRADAS:")
 let qt_apples = rs.questionInt(" >> ")
 
 // calcule how much pay for apples
