2cac468413ceadc065e896112480b5cc0e07ae78
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-09 18:13:37 -0300

    add input to ask price and percent

--- Atividades Condicional Avançada (18-07-2022)/Atividade4.js
+++ Atividades Condicional Avançada (18-07-2022)/Atividade4.js
@@ -1,3 +1,12 @@
 // Atividade Node.js
 // 4 - Solicite o preço de uma mercadoria e o percentual de desconto 20%. Exiba no console o valor do desconto e o preço a pagar.
 
+// import modules
+const rs = require("readline-sync")
+
+console.log("Digite o preço da mercadoria: R$")
+let valor = rs.questionInt(">>")
+
+console.log("Digite quantos porcentos (%) de desconto: ")
+let percent = rs.questionInt(">>")
+
