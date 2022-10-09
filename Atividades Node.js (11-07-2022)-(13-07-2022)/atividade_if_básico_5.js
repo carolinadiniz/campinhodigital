d81134b303cdd589f5ba11fd6c0864e0e4097b60
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-09 16:52:32 -0300

    push numbers to array

--- Atividades Node.js (11-07-2022)-(13-07-2022)/atividade_if_básico_5.js
+++ Atividades Node.js (11-07-2022)-(13-07-2022)/atividade_if_básico_5.js
@@ -7,6 +7,13 @@ const bar = require("./bar")
 
 // ask user for 3 numbers
 console.log("Digite 3 números inteiros: ")
-let num1 = rs.questionInt(" 1 - >>")
-let num2 = rs.questionInt(" 2 - >>")
-let num3 = rs.questionInt(" 3 - >>")
+let num1 = rs.questionInt(" >>")
+let num2 = rs.questionInt(" >>")
+let num3 = rs.questionInt(" >>")
+
+// push numbers to array
+numbers = []
+numbers.push(num1)
+numbers.push(num2)
+numbers.push(num3)
+
