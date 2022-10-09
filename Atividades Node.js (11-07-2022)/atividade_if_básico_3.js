493fdb08b7a6af92a0e9402c68a13dd80fa7fb26
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-09 14:49:21 -0300

    Create atividade_if_básico_3.js

---
+++ Atividades Node.js (11-07-2022)/atividade_if_básico_3.js
@@ -0,0 +1,7 @@
+// Atividade Node.js
+// 3 - Escreva um programa que verifique a validade de uma senha fornecida pelo usuário.
+// A senha válida é o número 1234.
+// Devem ser impressas as seguintes mensagens:
+// - ACESSO PERMITIDO, caso a senha seja válida.
+// - ACESSO NEGADO, caso a senha seja inválida.
+
