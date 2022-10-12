2d6174a8e47a0bb2c9a9880972f432f5c4850d92
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-12 13:43:36 -0300

    Create Atividade2.js

---
+++ Atividades While (25-07-2022)/Atividade2.js
@@ -0,0 +1,3 @@
+// Atividade While
+// 2 - Faça um programa que leia dois números, o primeiro é o valor inicial de um contador, e o segundo é o valor final do contador (Verifique se o valor inicial é inferior ao valor final). Usando o comando For, escreva na tela uma cotagem que comece no primeiro número lido, escreva os números seguintes colocando apenas um número em cada nova linha da tela, até chegar ao valor indicado.
+
