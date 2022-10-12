6331f38d03c93cec573b56469093915c849fd6f3
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-12 13:40:21 -0300

    create a for to replace letters

--- Atividades While (25-07-2022)/Atividade1.js
+++ Atividades While (25-07-2022)/Atividade1.js
@@ -5,4 +5,29 @@ const rs = require('readline-sync')
 
 console.log("Digite uma frase:")
 let sentense = rs.question(">>")
+let new_sentense = ""
 
+for(let i=0; i<sentense.length;i++) {
+    switch (sentense.charAt(i).toLowerCase()) {
+        case "a":
+            new_sentense+= "A"
+            break;
+        case "e":
+            new_sentense+= "E"
+            break;
+        case "i":
+            new_sentense+= "I"
+            break;
+        case "o":
+            new_sentense+= "O"
+            break;
+        case "u":
+            new_sentense+= "U"
+            break;
+        default:
+            new_sentense+=sentense.charAt(i).toLowerCase()
+            break;
+        }
+}
+
+console.log(new_sentense)
