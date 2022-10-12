39cb09cd8ae9659977401bc40cec3e29a9473bd5
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-12 13:33:50 -0300

    get user input

--- Atividades While (25-07-2022)/Atividade1.js
+++ Atividades While (25-07-2022)/Atividade1.js
@@ -1,3 +1,8 @@
 // Atividade While
 // 1 -Faça um algoritmo que receba uma entrada do usuário e caso encontre uma vogal transforme ela em maiúscula (Obrigatório usar FOR)
 
+const rs = require('readline-sync')
+
+console.log("Digite uma frase:")
+let sentense = rs.question(">>")
+
