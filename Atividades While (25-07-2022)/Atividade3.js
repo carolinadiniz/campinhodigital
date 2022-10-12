98d8a3597f965e3b28cbc2f8731782965f71b210
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-10-12 14:06:53 -0300

    Create Atividade3.js

---
+++ Atividades While (25-07-2022)/Atividade3.js
@@ -0,0 +1,3 @@
+// Atividade While
+// 3 - Faça um programa que escreva na tela os números múltiplos de 5 entre 0 e 1000, usando um comando FOR. Não utilize nenhum IF/THEN neste programa, apenas o comando for.
+
